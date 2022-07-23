f8fe3e1bc5d89a87dbf39917ae2229860fbb19f7
Matthew Merritt <matthew.merritt@yahoo.com>
2022-07-23 00:45:23 -0700

    Updated Package.swift to tools 5.6 iOS 15 macOS 12

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "MapCache",
     platforms: [
-        .iOS(.v14), .macOS(.v11)
+        .iOS(.v15), .macOS(.v12)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
