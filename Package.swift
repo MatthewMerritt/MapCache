64686adf89ce94706f74911663ef79e3926b3c58
Matthew Merritt <matthew.merritt@yahoo.com>
2022-07-23 00:21:42 -0700

    Updated Package.swift to iOS 14 and MacOS 11 swift-tools-verstion 5.3

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "MapCache",
     platforms: [
-        .iOS(.v8), .macOS(.v10_10)
+        .iOS(.v14), .macOS(.v11)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
